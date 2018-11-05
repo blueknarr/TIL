1ff2287d5b18a1c733815e359916cfcb207820a7
confortblack <confortblack@gmail.com>
2018-11-05 14:49:44 +0900

    set giphy API

--- js/giphy-search-engine/main.js
+++ js/giphy-search-engine/main.js
@@ -8,6 +8,7 @@ button.addEventListener('click',() => {
     pushToDOM(inputValue);
 });
 
+//enter key를 눌렀을 때 이벤트를 잡는다.
 inputArea.addEventListener('keyup',(e) =>{
     const inputValue = document.querySelector('#js-input').value;
     if(e.which ===13){
@@ -16,8 +17,10 @@ inputArea.addEventListener('keyup',(e) =>{
 });
 
 //2. API를 활용하여 data를 받는다. 가공한다.
-
-
+const API_KEY = 'XgPTvyw9a8ogUJdDfyjCun5HBDEAzX2c';
+let keyword = 'dogs';
+const URL = `api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${API_KEY}`;
+console.log(URL);
 //3. GIF 파일들을 index.html 에 밀어 넣는다.
 const pushToDOM = (data) => {
    const resultArea = document.querySelector('#result-area');
