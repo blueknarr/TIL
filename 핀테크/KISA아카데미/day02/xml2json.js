25161c6626a8cfc4edf176c448430b53d9078f80
Jeong Yong Su <knarrblue@gmail.com>
2019-05-21 15:20:38 +0900

    add request

--- 핀테크/KISA아카데미/day02/xml2json.js
+++ 핀테크/KISA아카데미/day02/xml2json.js
@@ -5,5 +5,5 @@ request('http://www.weather.go.kr/weather/forecast/mid-term-rss3.jsp?stnld=109',
     //var statusCode = convert.xml2json(response, {compact:true,spaces:4});
     var bodyCode = convert.xml2json(body,{compact:false,spaces:4});
     console.log(error);
-    console.log(bodyCode.rss.channel);
+    console.log(bodyCode);
 });
