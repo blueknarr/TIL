1bd4d5661655ff6d8e47a98f74a1213c14a56923
Jeong Yong Su <knarrblue@gmail.com>
2019-05-22 17:00:45 +0900

    add auth authentication

--- 핀테크/KISA아카데미/day03/app.js
+++ 핀테크/KISA아카데미/day03/app.js
@@ -8,30 +8,87 @@ var connection = mysql.createConnection({
     password : '1234',
     database : 'fintech' //database name (not table)
 });
-
+connection.connect();
 app = express();
 
 //css 경로 지정
 app.use(express.static(__dirname + '/public'));
+
+//json file parsing
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
 var port = process.env.PORT || 8080;
-app.listen(port);
+
 console.log("Listening on port ", port);
 
+//index page
 app.get('/',function(req, res){
     res.render('index');
 })
 
-var sql = 'SELECT * FROM fintech.user';
-app.get('/user',function(req,res){
-    connection.connect();
-    connection.query(sql,function(error,results){
+//join page
+app.get('/join',function(req, res){
+    res.render('join');
+})
+
+//토큰 받아오기
+app.get('/authResult',function(req, res){
+    var auth_code = req.query.code;
+    var getTokenUrl = "https://testapi.open-platform.or.kr/oauth/2.0/token";
+    var option = {
+        method : "POST",
+        url :getTokenUrl,
+        headers : {
+        },
+        form : {
+            code : auth_code,
+            client_id : "l7xx8f5b86755854495c87fcae22754baf59",
+            client_secret : "5d6d1f909731491196d6a5514d96deb5",
+            redirect_uri : "http://localhost:8080/authResult",
+            grant_type : "authorization_code"
+        }
+    };
+
+    request(option, function (error, response, body) {
         if(error) throw error;
-            res.send(results);
+        else{
+            console.log(body)
+            var accessRequestResult = JSON.parse(body);
+            console.log(accessRequestResult);
+            res.render('resultChild',{data : accessRequestResult});
+        }
+        
+        
     });
-    connection.end();
+})
+
+//view에서 입력받아 벡으로 전송
+app.post('/join',function(req,res){
+    
+    console.log(req);
+    var name = req.body.name;
+    var birth = req.body.birth;
+    var email = req.body.email;
+    var accessToken = req.body.accessToken;
+    var refreshToken = req.body.refreshToken;
+    var userNum = req.body.userNum;
+    var password = req.body.password;
+    var phone = req.body.phone;
+    
+    console.log(name, email, password);
+    var sql = 'INSERT INTO `fintech`.`user` (`name`, `birth`, `user_id`, `user_password`, `phone`, `accessToken`,`refreshToken`,`userNum`) VALUES (?,?,?,?,?,?,?,?);';
+    connection.query(sql,[name,birth,email,password,phone,accessToken,refreshToken,userNum],function(error,results){
+        if(error) throw error;
+        else{
+            console.log(this.sql);
+            res.json(1);
+        }
+    });
 });
+
+app.listen(port);
